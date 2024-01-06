4a6304bbe1d0edafe73c3a837e993c8c914bd08e
satriah <satriahazhari0101@gmail.com>
2024-01-06 07:53:37 +0700

    pos

--- database/migrations/2024_01_06_001602_create_transactions_table.php
+++ database/migrations/2024_01_06_001602_create_transactions_table.php
@@ -13,6 +13,24 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->integer('number');
+            $table->string('type_payment');
+            $table->integer('sub_total');
+            $table->integer('discount');
+            $table->integer('total');
+            $table->integer('pay');
+            $table->integer('change');
+            $table->timestamps();
+        });
+
+        Schema::create('transaction_items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('transaction_id');
+            $table->unsignedBigInteger('product_id');
+            $table->string('price');
+            $table->integer('qty');
+            $table->foreign('transaction_id')->references('id')->on('transactions');
+            $table->foreign('product_id')->references('id')->on('products');
             $table->timestamps();
         });
     }
@@ -23,5 +41,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('transactions');
+        Schema::dropIfExists('transaction_items');
+
     }
 };
